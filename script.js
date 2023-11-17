174c8909fd9d9eeb10153ef68f3bf6f07a0f6e3c
Trong Nhan <21521214@gm.uit.edu.vn>
2023-11-17 21:13:00 +0700

    add function addtocart, add function getProducts

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+//add product to localStorage
+function addtocart(curr) {
+    let products=[];
+    let img=curr.parentElement.children[5].children[0].src;
+    let productname=curr.parentElement.children[2].textContent;
+    let productprice=curr.parentElement.children[3].textContent;
+    let productquan=curr.parentElement.children[10].children[1].textContent;
+    let producttotal=parseFloat(productquan)*parseFloat(productprice.slice(1));
+    
+    if (localStorage.length!=0 ) {
+        products.push(...JSON.parse(localStorage.getItem("products")));
+      }
+      products.push({
+        img:img,
+        productname:productname,
+        productprice:productprice,
+        productquan:productquan,
+        producttotal:producttotal
+      });
+      localStorage.setItem("products", JSON.stringify(products));
+      getProducts()
+}
+
+
+//get product from localStorage
+function getProducts() {
+    document.getElementsByClassName('cart-products')[0].innerHTML='';
+    if(localStorage.length==0) {
+        document.getElementsByClassName('cart-products')[0].innerHTML+='<div style="font-size: 48px;text-align: center;padding: 75px 0px;">Empty</div>'
+    }
+    else {
+        let total=0;
+        document.getElementsByClassName('cart-products')[0].innerHTML+=`
+                <div class="cart-headings">
+                    <div class="product-detail-heading">Product</div>
+                    <div class="product-total-headings">
+                        <div class="product-price-heading">Price</div>
+                        <div class="product-quantity-heading">Quantity</div>
+                        <div class="product-total-heading">Total</div>
+                    </div>
+                </div>
+        `
+        let products=JSON.parse(localStorage.getItem("products"));
+        products.forEach(product => {
+            total+=product.producttotal;
+            document.getElementsByClassName('cart-products')[0].innerHTML+=`
+            <div class="cart-product">
+                            <div class="product-detail">
+                                <img src=${product.img} alt="" class="product-image">
+                                <div>
+                                    <p class="product-name">${product.productname}</p>
+                                    <p class="product-status">Remove</p>
+                                </div>
+                            </div>
+
+                            <div class="product-total">
+                                <div class="product-price">${product.productprice}</div>
+                                <div class="product-quantity">${product.productquan}</div>
+                                <div class="product-total-price">$${product.producttotal}</div>
+                            </div>
+            </div>`
+        })
+        
+        document.getElementsByClassName('cart-products')[0].innerHTML+=`
+        <div class="cart-sub-total">
+                        <div class="sub-total">
+                            <div class="sub-total-title">Sub-total</div>
+                            <div class="sub-total-price">$${total}</div>
+                            <div class="sub-total-note">Tax and shipping will be calculated later</div>
+                        </div>
+
+                        <div class="check-out-button" >
+                            <button onclick="CheckSectionDisplay(5)"><span></span>Check-out</button>
+                        </div>
+        </div>
+        `
+    }
+}
+
 // Check email
 var isEmailFilled = false; // Trạng thái nhập liệu email
 function checkEmail() {
@@ -52,6 +131,7 @@ function CheckSectionDisplay(num) {
     
     for (var i = 0; i < array.length; i++) {
         if (i === num) {
+            if(num==4) { getProducts()};
             document.getElementById(array[i]).style.display = 'block';
             document.getElementById(array[i]).scrollIntoView({ behavior: 'smooth', block: 'start' });
         } else {
@@ -59,6 +139,10 @@ function CheckSectionDisplay(num) {
         }
     }
 }
+
+
+
+
 // Hide all sections except for homepage section
 /*
 window.addEventListener('load', function() {
