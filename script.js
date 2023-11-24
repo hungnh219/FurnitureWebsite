ae1c068fe8da90fab3beda9a145385a48c250bdb
hitmnh <tttnhu02122@gmail.com>
2023-11-24 23:38:52 +0700

    section 3

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//script for slider homepage
+const imgPosition = document.querySelectorAll(".slider-image img")
+    const imgContainer = document.querySelector('.slider-image')
+    let index = 0;
+    let imgNumber = imgPosition.length
+    let space = 100
+    imgPosition.forEach(function(image, index) {
+        image.style.left = index*space + "%"
+    })
+    function imgSlide() {
+        index++;
+        console.log(index)
+        if(index >= imgNumber)
+        {
+            index = 0;
+            imgPosition.forEach(function(image, index) {
+            image.style.left = index*space + "%"})
+    }
+    imgContainer.style.left = "-" + index*space + "%"
+    
+}
+
+    setInterval(imgSlide, 5000)
+
 //add product to localStorage
 function addtocart(curr) {
     let products=[];
